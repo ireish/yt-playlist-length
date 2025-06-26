c5c0f01c96934d570869224f563215445edfec46
Elon Musk <ireish@hotmail.com>
2025-06-26 01:45:32 -0700

    fix: clear previous state on invalid url

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -44,6 +44,7 @@ const HomePage: React.FC = () => {
       
       setIsLoading(true);
       setError(null);
+      setPlaylistInfo(null);
       setVideos([]);
       setNextPageToken(null);
       
